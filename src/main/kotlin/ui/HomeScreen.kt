4138a798bd40b3bf83fe4a085225f7f673d6c296
Jorerock <jorerock@laposte.net>
2025-02-24 17:24:57 +0100

    Depart construction menu principal

--- src/main/kotlin/ui/HomeScreen.kt
+++ src/main/kotlin/ui/HomeScreen.kt
@@ -6,25 +6,36 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import ui.LoginScreen
+import routing.Router
+import routing.Routes
+
 
 @Composable
 @Preview
 fun HomeScreen() {
+    val router = remember { Router() }
+
     Column {
         Text("Coucou")
 
         Button(
             onClick = {
-           true},
+                print("Go to HOME")
+                router.navigateTo(route = Routes.HOME)
+            },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Suivis des colis")
         }
         Button(
             onClick = {
-                true},
+                print("Go to LOGIN")
+
+                router.navigateTo(route = Routes.LOGIN)
+            },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Gestion structure")
